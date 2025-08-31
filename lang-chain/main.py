3dcf114a2451864d79b08a013d365acacf65da63
danieltonad <soladan2000@gmail.com>
2025-09-01 00:05:21 +0100

    Add initial configuration, requirements, and .gitignore for project setup

---
+++ lang-chain/main.py
@@ -0,0 +1,18 @@
+from langchain.llms.openai import OpenAI
+import streamlit as st
+
+
+st.title("LangChain with Streamlit")
+input_text = st.text_input("Search the topic u want;")
+
+
+llm = OpenAI(temperature=0.9)
+
+if input_text:
+    response = llm.chat.completions.create(
+        model="gpt-4",
+        messages=[
+            {"role": "user", "content": input_text}
+        ]
+    )
+    st.write(response.choices[0].message.content)
